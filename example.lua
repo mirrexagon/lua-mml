95c7952b3cf6ab4bb3f1b7195d6266785ecf2ae2
LegoSpacy <legospacy@gmail.com>
2014-05-01 18:26:58 +1000

    Updated the example
    It was updated for the "outputType is an argument" change.

--- example.lua
+++ example.lua
@@ -1,14 +1,12 @@
 -- An example for the Lua MML module.
--- It relies on beep (found on Linux systems) to make sounds.
--- If you have beep and it doesn't work, try "sudo modprobe pcspkr"
--- That will try to load the "pcspkr" kernel module.
+-- It relies on the "play" from SoX (http://sox.sourceforge.net/).
 
 -- Load the module.
 local mml = require("mml")
 
 -- Sample songs!
 local twinkle = "t120 l4 o4  ccggaag2 ffeeddc2 ggffeed2 ggffeed2 ccggaag2 ffeeddc2"
-local canon = "t90 l16 o5  a8f#g a8f#g a<ab>c#def#g f#8de f#8<f#gabagaf#ga"
+local canon = "t110 l16 o5  a8f#g a8f#g a<ab>c#def#g f#8de f#8<f#gabagaf#ga"
 
 -- A simple busy-wait delay function.
 local clock = os.clock
@@ -17,25 +15,28 @@ function delay(n)
 	repeat until (clock() - start) >= n
 end
 
--- Since beep uses frequencies, output the frequency of each note.
-mml.outputType = "frequency"
+-- Create the player. Since SoX's synth effect uses frequencies, output the frequency of each note.
+local player = mml.newPlayer(canon, "frequency")
 
--- Create the player.
--- Change the song if you want!
-local player = mml:newPlayer(canon)
 
--- And loop!
 while true do
   -- Resume the coroutine to get the next note or rest.
 	local ok, note, time, vol = coroutine.resume(player)
 
-  -- If it"s finished, the coroutine will raise an error,
+  -- If it's finished, the coroutine will raise an error,
   -- which is caught by coroutine.resume and makes "ok" false.
-	if not ok then break end
+	-- The error message goes to "note".
+	if not ok then
+		print(note)
+		break
+	end
 
 	if note then
-    -- Use beep to sound the note!
-		os.execute("beep -f " .. note .. " -l " .. time * 1000)
+    -- Use SoX's synth effect to sound the note.
+		os.execute( string.format(
+			"play -qn -V0 synth %.2f pluck %.2f",
+			time, note
+		))
 	else
     -- If "note" is nil, it"s a rest.
 		delay(time)
